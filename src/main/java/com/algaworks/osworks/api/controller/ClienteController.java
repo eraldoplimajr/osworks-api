c8065c9f4c4962ec20d4fbd21ad0d7bdcfb5a74a
eraldoplimajr <eraldoplimajr@gmail.com>
2021-05-30 02:09:03 -0300

    Criando o ClienteRepository e utilizando o Spring Data JPA.

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,15 +1,13 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 /**
  *
@@ -19,12 +17,12 @@ import com.algaworks.osworks.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	@Autowired
+	ClienteRepository clienteRepository;
 	
 	@GetMapping("clientes")
 	public List<Cliente> listar() {		
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+		return clienteRepository.findAll();
 	}
 
 }
