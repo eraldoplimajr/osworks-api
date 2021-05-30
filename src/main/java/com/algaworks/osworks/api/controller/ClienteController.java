950e9ffafc4f2d1d0350312e62a4fbd97a66f7fc
eraldoplimajr <eraldoplimajr@gmail.com>
2021-05-30 20:23:25 -0300

    Adicionando endpoint buscar cliente por identificador

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,9 +1,12 @@
 package com.algaworks.osworks.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
@@ -20,9 +23,19 @@ public class ClienteController {
 	@Autowired
 	ClienteRepository clienteRepository;
 	
-	@GetMapping("clientes")
+	@GetMapping("/clientes")
 	public List<Cliente> listar() {		
 		return clienteRepository.findAll();
 	}
-
+	
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
 }
