95493e77a012d73a3e138d15e9b6c888c45bba55
eraldoplimajr <eraldoplimajr@gmail.com>
2021-05-30 01:49:47 -0300

    Realizando consulta de todos os clientes com JPA.

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.algaworks.osworks.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,22 +19,12 @@ import com.algaworks.osworks.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("clientes")
-	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Joao");
-		cliente1.setTelefone("34 99999-1111");
-		cliente1.setEmail("joaodascouves@algaworks.com");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("11 77777-1111");
-		cliente2.setEmail("mariadasilva@algaworks.com");
-		
-		
-		return Arrays.asList(cliente1, cliente2);
+	public List<Cliente> listar() {		
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 
 }
