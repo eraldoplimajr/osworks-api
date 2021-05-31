95218a51ae7c02396170be70c25e7b7e07a9b6b4
eraldoplimajr <eraldoplimajr@gmail.com>
2021-05-30 22:34:26 -0300

    Realizando tratamento de exceção.

---
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.algaworks.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ *
+ * @author Eraldo Lima
+ *
+ */
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setDataHora(LocalDateTime.now());
+		problema.setTitulo("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.");
+		
+		return super.handleExceptionInternal(ex, problema, headers, status, request);
+	}
+	
+}
